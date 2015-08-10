5a22946d63e851e3d805aa7544f0b8ef9226d8d0
Juan Llado <jllado@gmail.com>
2015-08-10 19:57:41 +0200

    Remove unused import

--- src/main/java/juanllado/java8lambdasmooc/Lesson2.java
+++ src/main/java/juanllado/java8lambdasmooc/Lesson2.java
@@ -13,7 +13,6 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
